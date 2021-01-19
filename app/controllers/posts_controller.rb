48f1778c539fcafe21ab5d6c4b46f4115c81a653
github username <github email address>
2021-01-19 01:42:03 -0600

    adds crud methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+        
+        render json: @posts
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def post_params
+    end
 end
